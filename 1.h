6af962ee778d3f887825460768ea75636b74f376
谢庆集 <2847843990@qq.com>
2023-06-12 20:48:21 +0800

    ggggggggggg

---
+++ 1.h
@@ -0,0 +1,27 @@
+#if COMPRESS_MONO_FONT
+  extern code BYTE tOSDFont1[156];
+  extern code WORD tMonoFontDataAddr[6];
+#else // No compress
+  extern code WORD tOSDFont1[108]; // Format 2: 1 word ==> 1 line
+
+#endif
+#ifndef __COLOR_FONT_H
+#define __COLOR_FONT_H
+
+  #ifdef COMPRESS_COLOR_FONT
+    extern code BYTE tCompressColorFontData[0];
+  #else
+    extern code BYTE tColorFontData[0];
+  #endif
+
+#endif
+#ifndef __COLOR_FONT_H
+#define __COLOR_FONT_H
+
+  #ifdef COMPRESS_COLOR_FONT
+    extern code BYTE tCompressColorFontData[0];
+  #else
+    extern code BYTE tColorFontData[0];
+  #endif
+
+#endif
